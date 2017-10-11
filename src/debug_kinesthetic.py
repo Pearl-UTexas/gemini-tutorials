365cedafc40cb902bfeff42b0c0f9ce10ddaeb80
Ajinkya Jain <jainajinkya92@gmail.com>
2017-10-11 17:21:02 -0500

    sync

--- src/debug_kinesthetic.py
+++ src/debug_kinesthetic.py
@@ -18,6 +18,21 @@ def traj_callback(msg):
 	global joint_traj
 	joint_traj = copy.deepcopy(msg.trajectory[0])
 
+def clean_joint_traj(msg):
+	msg = copy.deepcopy(msg)
+	for pt in msg.points:
+		pt.velocities = []
+		pt.accelerations = []
+		pt.effort = []
+
+	return msg
+
+def tweak_time(msg, time_scale):
+	msg = copy.deepcopy(msg)
+	for pt in msg.points:
+		pt.time_from_start /= 1.*time_scale
+	return msg
+
 def main():
 	global joint_traj
 	arm = ArmMoveIt(planning_frame='linear_actuator_link', _arm_name='right')
@@ -37,23 +52,29 @@ def main():
 	raw_input('Executing Arm trajectory command. Press Enter!')
 	jointTarget = end
 	arm.move_to_joint_pose(jointTarget)
-	rospy.sleep(7)
+	rospy.sleep(5)
 
 	# Save joint trajectory
-	joint_traj_saved = copy.deepcopy(joint_traj.joint_trajectory)
+	joint_traj_saved = clean_joint_traj(joint_traj.joint_trajectory)
 
 	# Going Back for Playback
 	jointTarget = start
-	arm.move_to_joint_pose(jointTarget)
-	rospy.sleep(5)
+	for i in range(3):
+		arm.move_to_joint_pose(jointTarget)
+		rospy.sleep(1)
+		print 'Iterating over start point'
 
-	# Do FK for every point in traj to convert to eef trajectory
+	rospy.sleep(3)
+
 	current_joint_pt = JointTrajectoryPoint()
 	current_joint_pt.positions = arm.get_current_pose()
-	joint_traj_saved.points = [current_joint_pt] + joint_traj_saved.points
-	for i in range(1,len(joint_traj_saved.points)):
-		joint_traj_saved.points[i].time_from_start.nsecs += 500000000
+	# joint_traj_saved.points = [current_joint_pt] + joint_traj_saved.points
+	joint_traj_saved.points[0] = current_joint_pt
+	# for i in range(1,len(joint_traj_saved.points)):
+	# 	joint_traj_saved.points[i].time_from_start.nsecs += 500000000
 
+
+	# Do FK for every point in traj to convert to eef trajectory
 	ee_traj = []
 
 	for i, pt in enumerate(joint_traj_saved.points):
@@ -76,8 +97,16 @@ def main():
 	raw_input('Playing back the trajectory, Press Enter!')
 	#for ee_pose in ee_traj:
 	#	arm.move_to_ee_pose(ee_pose.pose)
-	#arm.execute_ee_trajectory(ee_traj)
-	arm.execute_joint_trajectory(joint_traj_saved)
+
+	# print 'Going to the first ee pose in ee traj'
+	# joint_traj = arm._ee_traj_to_joint_traj(ee_traj)
+	# arm.move_to_joint_pose(joint_traj.points[0].positions)
+	# print 'Exercuting the rest traj'
+	# arm.execute_joint_trajectory(joint_traj)
+
+	arm.execute_ee_trajectory(ee_traj, use_custom_time=False)
+	#joint_traj_saved = tweak_time(joint_traj_saved, 2)
+	#arm.execute_joint_trajectory(joint_traj_saved, use_custom_time=True)
 
 	# Shutdown
 	print "shutting down!"
